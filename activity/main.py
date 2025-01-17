a1dbd33ee451487772703cd85a8073b2c4a15616
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2025-01-17 13:45:22 -0600

    Added main.py skeleton and tests in test_kth_smallest.py

---
+++ activity/main.py
@@ -0,0 +1,20 @@
+class TreeNode:
+
+    def __init__(self, key, left=None, right=None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+
+def kth_smallest(root, k):
+    """
+    This function takes in the root of the tree and an integer k and returns the kth smallest value in the tree.
+  
+    Parameters:
+    root (TreeNode): The root of the tree
+    k (int): represents the kth smallest value which we would like to find
+  
+    Returns:
+    int: the kth smallest value
+    """
+    pass
---
+++ tests/test_kth_smallest.py
@@ -0,0 +1,36 @@
+from activity.main import TreeNode, kth_smallest
+import pytest
+
+def test_kth_smallest_on_valid_bst():
+    #Arrange 
+    root = TreeNode(6, TreeNode(3, TreeNode(1), TreeNode(5)),TreeNode(8, None, TreeNode(9)))
+    k = 3
+
+    #Act
+    result = kth_Smallest(root, k)
+
+    #Assert
+    assert result == 5
+
+def test_kth_smallest_with_less_balanced_bst():
+    #Arrange
+    root = TreeNode(5, TreeNode(3, TreeNode(1, TreeNode(0))),TreeNode(7, None, TreeNode(8)))
+    k = 5
+
+    #Act
+    result = kth_smallest(root, k)
+
+    #Assert
+    assert result == 7
+
+def test_kth_smallest_on_single_node_bst():
+    #Arrange
+    root = TreeNode(42, None, None)
+    k = 1
+
+    #Act
+    result = kth_smallest(root,k)
+
+    #Assert
+    assert result == 42
+
